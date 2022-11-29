1bb4eb6eb10b314886d19314109ec123c03e991e
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-11-29 10:17:28 +0100

    Get request api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
+const {
+    MongoClient
+} = require('mongodb');
+require('express-group-routes');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT;
+
+const client = new MongoClient(process.env.FINAL_URL);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.group("/api", (router) => {
+    router.get("/projects", async (req, res) => {
+        try {
+            await client.connect();
+
+            const coll = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION)
+            const data = await coll.find({}).toArray();
+
+            res.status(200).send(data)
+        } catch (err) {
+            res.status(500).send({
+                error: "Something went wrong",
+                value: err
+            })
+        } finally {
+            await client.close();
+        }
+    })
 })
 
 app.listen(port, () => {
